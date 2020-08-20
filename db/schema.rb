ccedbb9463551b7ba4d78e1095fe6f9124343f9a
kein.takahashi <keinfirstdirt1564@gmail.com>
2020-08-20 07:22:59 +0000

    "message" to "task"

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_091430) do
+ActiveRecord::Schema.define(version: 2020_08_13_091909) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "status"
   end
 
 end
